6181182693fc1544ad70a8f6b98d30fee6c48c9f
Theo Morin <theo.morin1402@gmail.com>
2025-01-14 14:00:04 +0100

    licence

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from distutils.core import setup
+setup(
+ name='{https://github.com/theo-mrn/TD1-2}',
+ version='1.0',
+ author='{theo-mrn}',
+ license='MIT',
+ long_description=open('README.md').read(),
+)
